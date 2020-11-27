6f11d795b58ea104a683fe0fcc2a5bf4bc024312
drajat824 <64979984+drajat824@users.noreply.github.com>
2020-11-27 09:46:20 +0700

    Update App.js

--- App.js
+++ App.js
@@ -5,13 +5,22 @@ import MainNavigator from './src'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/es/integration/react'
 import { store, persistor } from './src/redux/store'
+import {
+  SafeAreaView,
+  StatusBar,
+} from 'react-native';
+import {Welcome} from './src/screen'
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <PaperProvider>
-          <MainNavigator />
+           <StatusBar translucent barStyle='dark-content' backgroundColor="transparent"  />
+            <SafeAreaView>
+              <Welcome/>
+            </SafeAreaView>
         </PaperProvider>
       </PersistGate>
     </Provider>
