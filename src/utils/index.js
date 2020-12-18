a3c5ab8f0aa55d0a969e767598c467b50a1285e3
amyusup <amyusup26@gmail.com>
2020-12-18 16:24:51 +0700

    fix aws

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,13 @@
-export const URI = 'http://192.168.43.149:8000/api/v1';
-export const imageURI = 'http://192.168.43.149:8000/image/'
-export const baseURI = 'http://192.168.43.149:8000/'
+//aws
+
+export const URI = 'http://18.212.1.32:8000/api/v1';
+export const imageURI = 'http://18.212.1.32:8000/image/'
+export const baseURI = 'http://18.212.1.32:8000/'
+// heroku
+// export const URI = 'https://ankasa-api.herokuapp.com/api/v1';
+// export const imageURI = 'https://ankasa-api.herokuapp.com/image/'
+// export const baseURI = 'https://ankasa-api.herokuapp.com/'
+//lokal
+// export const URI = 'http://192.168.43.149:8000/api/v1';
+// export const imageURI = 'http://192.168.43.149:8000/image/'
+// export const baseURI = 'http://192.168.43.149:8000/'
